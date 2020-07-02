2be5f6c825e5f0017b14d9be9c40188d09002792
nikabranickaepam <nika_branicka@epam.com>
2020-07-02 20:25:30 +0200

    Refactored my_split and extended with max_split

--- tasks/module/three/task_2.py
+++ tasks/module/three/task_2.py
@@ -1,22 +1,31 @@
 import pytest
+import re
 
 NO_SPLITTER_ERROR_MESSAGE = "Splitter is not provided"
 
 
-def my_split(string_to_split, splitter=' '):
+def my_split(string_to_split, splitter=None, max_split=-1):
     if splitter == "":
         raise ValueError(NO_SPLITTER_ERROR_MESSAGE)
 
     split_result = []
-    text = ''
-    for character in string_to_split:
-        if character == splitter:
-            split_result.append(text)
-            text = ''
+
+    if splitter is None:
+        splitter = ' '
+        string_to_split = re.sub('[ \t\n]+', ' ', string_to_split)
+
+    while len(string_to_split) >= len(splitter) and max_split != 0:
+        splitter_position = string_to_split.find(splitter)
+        if splitter_position == -1:
+            split_result.append(string_to_split)
+            string_to_split = ''
+            break
         else:
-            text = text + character
-    if not text == '':
-        split_result.append(text)
+            split_result.append(string_to_split[0:splitter_position])
+            string_to_split = string_to_split[splitter_position + len(splitter):]
+            max_split -= 1
+    if len(string_to_split) > 0:
+        split_result.append(string_to_split)
     return split_result
 
 
@@ -36,8 +45,17 @@ def test_my_split(text_to_split, splitter):
                           ('a b c d', ' ', ['a', 'b', 'c', 'd']),
                           ('%*^*&', '*', ['%', '^', '&']),
                           ('ab ba', ' ', ['ab', 'ba'])])
-def test_my_split_basic_split(text_to_split, splitter, expected_result):
+def test_my_split_with_defined_splitter(text_to_split, splitter, expected_result):
     assert my_split(text_to_split, splitter) == expected_result
+
+
+@pytest.mark.parametrize("text_to_split,max_split,expected_result",
+                         [('a b c', 2, ['a', 'b', 'c']),
+                          ('a b c', 1, ['a', 'b c']),
+                          ('a b c', 0, ['a b c']),
+                          ('a b c', 4, ['a', 'b', 'c'])])
+def test_my_split_with_defined_count(text_to_split, max_split, expected_result):
+    assert my_split(text_to_split, max_split=max_split) == expected_result
 
 
 @pytest.mark.parametrize("text_to_split,expected_result",
