a84ca11971d9b813f7dcb4428fb084dfa11723a7
nikabranickaepam <nika_branicka@epam.com>
2020-07-28 18:48:48 +0200

    Updated task 6

--- tasks/module/four/task_six/task_6.py
+++ tasks/module/four/task_six/task_6.py
@@ -6,13 +6,13 @@ import PyPDF4
 PDF_FILE = 'SecretFile.pdf'
 POSSIBLE_PASSWORDS = 'dictionary.txt'
 
-file_text = open(POSSIBLE_PASSWORDS, 'r').read()
-list_of_possible_passwords = file_text.split()
+passwords = open(POSSIBLE_PASSWORDS, 'r').read()
+list_of_possible_passwords = passwords.split()
 
 pdfReader = PyPDF4.PdfFileReader(open(PDF_FILE, 'rb'))
 
 print('The password is:')
-for word in file_text.split():
+for word in passwords.split():
     if pdfReader.decrypt(word.lower()) == 1:
         print(word.lower())
         break
