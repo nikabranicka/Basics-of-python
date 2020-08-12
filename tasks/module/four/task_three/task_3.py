030269e19c55167151ef7a2c392f09cb0b92a9df
nikabranickaepam <nika_branicka@epam.com>
2020-08-12 21:11:10 +0200

    Fixed task 3 according to requirements

--- tasks/module/four/task_three/task_3.py
+++ tasks/module/four/task_three/task_3.py
@@ -2,31 +2,22 @@ import os
 import shutil
 import pyinputplus as pyip
 
+"""
+Code responsible for finding specified files in folder 'files' and coping them to provided destination
+"""
 
-def find_chosen_file_in_files_folder_location_and_copy_it_to_defined_location():
-    """
-    Method responsible for finding specifiied file in folder 'files' and coping it to provided destination
-    """
-    print("Please provide correct file name. Remember file must be inside repository in folder named 'files'")
-    print("IMPORTANT! provide file name along with extension")
+INPUT_FOLDER = "./files"
+print("Please provide extension of files which would you like to copy")
 
-    file_to_copy = pyip.inputStr()
-    if not os.path.exists(f'./files/{file_to_copy}'):
-        raise FileNotFoundError("Provided file name does not exist")
+file_extension_to_copy = pyip.inputStr()
 
-    print("Please provide destination folder name.")
-    print("Remember folder will be created inside repository under module/four/task_three if does not exist")
+print("Please provide destination folder name.")
+print("Remember folder will be created inside repository under module/four/task_three if does not exist")
+destination = pyip.inputStr()
+if not os.path.exists(f'./{destination}'):
+    os.mkdir(destination)
 
-    destination = pyip.inputStr()
-    if not os.path.exists(f'./{destination}'):
-        os.mkdir(destination)
-
-    shutil.copy2(f'files/{file_to_copy}', destination)
-
-
-def main():
-    find_chosen_file_in_files_folder_location_and_copy_it_to_defined_location()
-
-
-if __name__ == "__main__":
-    main()
+for dirpath, dirnames, files in os.walk(INPUT_FOLDER):
+    for file in files:
+        if file.endswith(file_extension_to_copy):
+            shutil.copy2(os.path.join(dirpath, file), destination)
